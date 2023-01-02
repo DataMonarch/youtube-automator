c6805d4022ebb3c40a810540cb9118d763b09aaf
DataMonarch <toghrultahirov2@gmail.com>
2023-01-02 11:52:47 +0400

    Added "video" folder and removed unnecessary clutter

---
+++ youtube_downloader.py
@@ -0,0 +1,20 @@
+from pytube import YouTube
+
+def Download(link):
+    yt = YouTube(link)
+    youtubeObject = yt.streams.get_highest_resolution()
+    captions = yt.captions.get_by_language_code('en')
+    captions = captions.generate_srt_captions()
+    success = False
+    try:
+        youtubeObject.download()
+        success = True
+        
+    except:
+        print("An error has occurred")
+        
+    return success, captions
+
+link = input("Enter the link of the video: ")
+_, captions = Download(link)
+print(captions)
