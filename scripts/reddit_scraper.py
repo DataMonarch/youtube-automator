6dcc97b8d98c56f8787cb259517a4f033b5a773d
DataMonarch <toghrultahirov2@gmail.com>
2023-01-03 10:41:41 +0400

    Modularized the reddit scraper and added the capability to scrape on thread or comment level

--- scripts/reddit_scraper.py
+++ scripts/reddit_scraper.py
@@ -1,6 +1,7 @@
 import tomli
 import json
 import praw
+import os
 
 with open("configs/reddit_config.toml", mode="rb") as fp:
     config = tomli.load(fp)
@@ -13,34 +14,51 @@ reddit = praw.Reddit(client_id=CLIENT_ID,
                      client_secret=CLIENT_SECRET,
                      user_agent='discussion_scraper')
 
-subreddit = reddit.subreddit('TalesFromRetail')
+def scrape_subreddit(subreddit_name, threads_no_limit=10, skip_n_submission=0,
+                     output_file_path=None, scraping_level="thread", comments_no_limit=5):
 
-submissions_dict = {}
-
-for i, submission in enumerate(subreddit.hot(limit=10)):
-    print(f'Hottest thread no.{i}: {submission.title}')
-    # get the id of a subreddit submission
-    flair = submission.link_flair_text
-    print(f"Link flair text: {flair}")
-    
-    if flair.lower() in TFR_FLAIRS:
-
-        comments = {comment.id: [comment.score, comment.body] for comment in submission.comments.list()}
-        if not comments:
-            top_comment = None
-        else:
-            top_comment = sorted(comments.items(), key=lambda x: x[1][0], reverse=True)[0] 
+    if not output_file_path:
+        output_file_path = f"submissions_{subreddit_name}"
         
-        submissions_dict[submission.id] = {'title': submission.title, 
-                                        'flair': flair,
-                                        'top_comment': top_comment}
-        print(top_comment)
-        print('*' * 20)
- 
+    subreddit = reddit.subreddit(subreddit_name)
 
-json_object = json.dumps(submissions_dict, indent=4)
+    if os.path.exists(output_file_path):
+        with open("../data/submissions_tfr.json") as f:
+            submissions_dict = json.load(f)
+    else:
+        submissions_dict = {}
+        
 
-with open('../data/submissions_tfr.json', 'a+') as f:
-    f.write(json_object)
+    for i, submission in enumerate(subreddit.top(limit = threads_no_limit)):
+        # print(f'Hottest thread no.{i}: {submission.title}')
+        # get the id of a subreddit submission
+        flair = submission.link_flair_text
+        # print(f"Link flair text: {flair}")
+        submission_id = submission.id
+        
+        if submission_id not in submissions_dict.keys():
+        
+            if flair.lower() in TFR_FLAIRS:
+                
+                if scraping_level == "comment":
+                    comments = {comment.id: [comment.score, comment.body] for i, comment in enumerate(submission.comments.list()) if i<comments_no_limit}
+               
+                # if not comments:
+                #     top_k_comments = None
+                # else:
+                #     top_k_comments = sorted(comments.items(), key=lambda x: x[1][0], reverse=True)[0] 
+                
+                submissions_dict[submission.id] = {'title': submission.title, 
+                                                'flair': flair,
+                                                'top_comment': top_comment}
+                # print(top_comment)
+                # print('*' * 20)
     
-# scrape the most liked comment under a thread in a subreddit
+
+    json_object = json.dumps(submissions_dict, indent=4)
+
+    with open('../data/submissions_tfr.json', 'a+') as f:
+        f.write(json_object)
+    
+# scrape the threads in a subreddit
+
