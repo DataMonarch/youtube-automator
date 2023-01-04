f495759302e623b273792db38c8b86158c8d0c34
DataMonarch <toghrultahirov2@gmail.com>
2023-01-04 20:23:30 +0400

    Now, can get the caption object from the youtube stream

--- scripts/youtube_downloader.py
+++ scripts/youtube_downloader.py
@@ -1,24 +1,37 @@
-from pytube import YouTube, extract
+from pytube import YouTube, extract, query
 import argparse
 import os
 
 def Download(url: str, save_path: str = '../data/videos'):
+    
+    save_path = os.path.abspath(save_path)
     yt = YouTube(url)
     video_id = extract.video_id(url)
     
+    filename = f'{video_id}.mp4'
+    file_path  = os.path.join(save_path, filename)
+    
     youtube_object = yt.streams.get_highest_resolution()
-    captions = yt.captions.get_by_language_code('en')
+    captions = yt.captions
     
     # TO-DO: pull captions as well
-    # captions = captions.generate_srt_captions()
+    
+    
+    
+    try:
+        captions = captions['en'].generate_srt_captions()
+    except KeyError:
+        print(f'No English captions found for {video_id}')
+        captions = None
+
     
     success = False
     try:
-        youtube_object.download(output_path=save_path, filename=video_id)
+        youtube_object.download(output_path=save_path, filename=filename)
         success = True
-        
+        print(f'>>> Downloaded {filename} as {file_path}')
     except:
-        print("An error has occurred")
+        print(f"! An error has occurred while downloading the video {video_id}")
         
     return success, captions
 
