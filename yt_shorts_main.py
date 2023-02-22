7f72e1aa97ed01fae998906f23303528732768e2
DataMonarch <toghrultahirov2@gmail.com>
2023-02-22 15:57:46 +0400

    Main function to get a number of short videos

---
+++ yt_shorts_main.py
@@ -0,0 +1,18 @@
+from video_processor import trimmer
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-vid", "--video_id", help="the id of the video to be trimmed")
+parser.add_argument("-c", "--count", help="the number of times the video should be trimmed")
+args = parser.parse_args()
+
+
+video_id = args.video_id
+count = args.count
+
+for i in range(count):
+    trimmer.get_video_clip(video_id)
+
+
+# a parser to get the arguments video_id and count from the command line and assign them to variables
