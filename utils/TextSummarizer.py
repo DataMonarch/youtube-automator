23e6104de75517586e03c973272b87888e1d807c
DataMonarch <toghrultahirov2@gmail.com>
2023-02-25 18:41:12 +0400

    It's now possible to programmatically select the sentences with the highest information values

--- utils/TextSummarizer.py
+++ utils/TextSummarizer.py
@@ -1,2 +1,23 @@
 # summarize a body of text by extracting the most information dense sentences
 
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lex_rank import LexRankSummarizer
+
+def summaize(text: str, num_resultatnt_sentences: int = None):
+
+    # Load text
+    if not num_resultatnt_sentences:
+        num_resultatnt_sentences = len(text.split('.'))
+
+    # Initialize parser and tokenizer
+    parser = PlaintextParser.from_string(text, Tokenizer("english"))
+
+    # Initialize summarizer
+    summarizer = LexRankSummarizer()
+
+    # Summarize text
+    summary = summarizer(parser.document, num_resultatnt_sentences) # 3 is the number of sentences to select
+
+    # Print summary
+    return summary
