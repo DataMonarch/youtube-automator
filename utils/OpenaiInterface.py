7960f112bf88164b01642a80ad41801793d2ff9a
DataMonarch <toghrultahirov2@gmail.com>
2023-02-26 19:02:09 +0400

    Trials on automatic grammar correction

--- utils/OpenaiInterface.py
+++ utils/OpenaiInterface.py
@@ -15,7 +15,7 @@ def ask_chatGPT(prompt):
     completions = openai.Completion.create(
         engine="text-davinci-003",
         prompt=prompt,
-        max_tokens=4000,
+        max_tokens=2048,
         n=1,
         stop=None,
         temperature=0.5,
