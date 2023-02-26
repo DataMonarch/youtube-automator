0b165690007eccca335bd50e1efbd22f0ec78237
DataMonarch <toghrultahirov2@gmail.com>
2023-02-26 15:57:22 +0400

    Now it's possible to pass a video url and start and end times to get a video clip

--- yt_shorts_main.py
+++ yt_shorts_main.py
@@ -1,4 +1,5 @@
 from video_processor import trimmer
+from video_downloader import youtube_downloader
 import argparse
 import json
 
@@ -8,16 +9,14 @@ with open("../data/videos/scraped_videos.json", "rb") as f:
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-l", "--url", help="the url of the video to be trimmed")
-parser.add_argument("-s", "--start", help="start time in the format hh.mm.ss")
-parser.add_argument("-e", "--end", help="start time in the format hh.mm.ss")
+parser.add_argument("-s", "--start", help="start time in the format hh.mm.ss", default=None)
+parser.add_argument("-e", "--end", help="start time in the format hh.mm.ss", default=None)
 parser.add_argument("-vid", "--video_id", help="the id of the video to be trimmed")
 parser.add_argument("-c", "--count", help="the number of times the video should be trimmed")
 args = parser.parse_args()
 
 if args.url or args.video_id:
-    if not args.start or not args.end:
-        print("Please specify the start and end times")
-    elif args.url:
+    if args.url:
         # extract the youtube video id from the url
         video_id = args.url.split("=")[1]
     elif args.video_id:
@@ -26,9 +25,16 @@ if args.url or args.video_id:
         raise ValueError("Please enter a valid url or video id")
     
 count = int(args.count)
+start_time = args.start
+end_time = args.end
+
+if not video_id in available_video_ids:
+    print(f">>> Video with id {video_id} not found in the available videos list. Downloading now...")
+    youtube_downloader.download(video_id)
 
 for i in range(count):
-    trimmer.get_video_clip(video_id)
-
+   
+    trimmer.get_video_clip(video_id, start_time, end_time)
+    
 
 # a parser to get the arguments video_id and count from the command line and assign them to variables
