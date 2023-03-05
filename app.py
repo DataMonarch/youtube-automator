17e7f867db7b8a7cb002276b8bcc83f3397393c4
DataMonarch <toghrultahirov2@gmail.com>
2023-03-05 10:31:29 +0400

    Trials on gui logging

--- app.py
+++ app.py
@@ -8,9 +8,18 @@ from video_processor import trimmer
 from video_downloader import youtube_downloader
 import json
 
+class GUI:
+    def __init__(self, master):
+        self.master = master
+        self.progress_text = tk.Text(master, height=20, width=50)
+        self.progress_text.pack()
 
+    def log_progress(self, message):
+        self.progress_text.insert(tk.END, message)
+        self.progress_text.see(tk.END)
+        self.master.update_idletasks()
 
-def get_video_clip():
+def get_video_clip(gui):
     video_url = url_input.get() 
     start_time = start_input.get()
     end_time = end_input.get()
@@ -27,7 +36,7 @@ def get_video_clip():
     
     
     if  video_id not in available_video_ids:
-        print(f">>> Video with id {video_id} not found in the available videos list. Downloading now...")
+        gui.log_progress(f">>> Video with id {video_id} not found in the available videos list. Downloading now...")
         youtube_downloader.download(video_url)
     
     # with open("../data/videos/scraped_videos.json", "r") as f:
@@ -35,58 +44,51 @@ def get_video_clip():
     #     available_video_ids = available_videos.keys()
     
     # if video_id in available_video_ids:
-    print(f">>> Done dowloading. Trimming now...")
-    trimmer.get_video_clip(video_id, start_time, end_time)
+    gui.log_progress(f">>> Done dowloading. Trimming now...")
+    try:
+        trimmer.get_video_clip(video_id, start_time, end_time)
+    except:
+        gui.log_progress(f">>> Error trimming video with id {video_id}")
+    else:
+        gui.log_progress(f">>> Done trimming video with id {video_id} from {start_time} to {end_time}")
+
+def main():
+    root = tk.Tk()
+    root.title("VIdeo Trimmer")
     
+    gui = GUI(root)
+    sys.stdout = gui
+    
+    url_label = tk.Label(root, text="Enter YouTube Video URL:")
+    url_label.pack()
 
-def redirect_output(function):
-    def wrapper(*args, **kwargs):
-        # redirect output to the Text widget
-        text_widget = kwargs.pop('text_widget', None)
-        if text_widget:
-            text_widget.configure(state="normal")
-            text_widget.delete('1.0', 'end')
-            original_stdout = sys.stdout
-            sys.stdout = redirected_output = StringIO()
-            result = function(*args, **kwargs)
-            sys.stdout = original_stdout
-            redirected_output = redirected_output.getvalue()
-            text_widget.insert('end', redirected_output)
-            text_widget.configure(state="disabled") 
-        else:
-            result = function(*args, **kwargs)
-        return result
-    return wrapper
+    url_input = tk.Entry(root)
+    url_input.pack()
+
+    start_label = tk.Label(root, text="Enter Start Time (hh.mm.ss):")
+    start_label.pack()
+
+    start_input = tk.Entry(root)
+    start_input.pack()
+
+    end_label = tk.Label(root, text="Enter End Time (hh.mm.ss):")
+    end_label.pack()
+
+    end_input = tk.Entry(root)
+    end_input.pack()
+
+    text_widget = ScrolledText(root)
+    text_widget.pack()
+      
+    submit_button = tk.Button(root, text="Submit", command=get_video_clip(gui))
+    submit_button.pack()
 
 
-@redirect_output
-def submit_action():
-    get_video_clip()
+    # Redirect stdout to the GUI text widget
+    root.mainloop()
 
-root = tk.Tk()
+if __name__ == "__main__":
+    main()
 
-url_label = tk.Label(root, text="Enter YouTube Video URL:")
-url_label.pack()
 
-url_input = tk.Entry(root)
-url_input.pack()
 
-start_label = tk.Label(root, text="Enter Start Time (in seconds):")
-start_label.pack()
-
-start_input = tk.Entry(root)
-start_input.pack()
-
-end_label = tk.Label(root, text="Enter End Time (in seconds):")
-end_label.pack()
-
-end_input = tk.Entry(root)
-end_input.pack()
-
-text_widget = ScrolledText(root)
-text_widget.pack()
-
-submit_button = tk.Button(root, text="Submit", command=submit_action(text_widget=text_widget))
-submit_button.pack()
-
-root.mainloop()
